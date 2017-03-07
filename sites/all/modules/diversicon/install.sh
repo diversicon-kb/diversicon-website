2a02d0a1b3ef9941c02f3b0598a6d2dccf245fde
DavidLeoni <davidleonigit-github@yahoo.com>
2017-03-07 09:55:58 +0100

    Redirecting with .htaccess
    - also redirecting /img  to diversicon overlay/img

--- sites/all/modules/diversicon/install.sh
+++ sites/all/modules/diversicon/install.sh
@@ -4,7 +4,3 @@ set -e
 # NOTE: The shell * DOES NOT* exit if the command that fails is part of the command 
 # list immediately following a while or until keyword, part of the test in an if 
 # statement, part of a && or || list, or if the command's return value is being inverted via !
-
-drush dl redirect-7.x-1.0-rc3
-
-drush -y en redirect
